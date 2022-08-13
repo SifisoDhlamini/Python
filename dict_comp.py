564b12fac64d2463e4d66b1790f8ca302e3a2e7c
Scifi_the_dev <sifisoldhlamini@outlook.com>
2022-08-13 23:39:26 +0800

    Dictionary comprehension

---
+++ dict_comp.py
@@ -0,0 +1,8 @@
+students = ["Hermione", "Harry", "Ron"]
+
+#gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
+gryffindors = {student: "Gryffindor" for student in students}
+
+print(gryffindors)
+
+
