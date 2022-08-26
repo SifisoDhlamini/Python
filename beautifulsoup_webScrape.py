9b150ae2aeec85945ad0dc4f9d4e1dec9ec6379c
Scifi_the_dev <sifisoldhlamini@outlook.com>
2022-08-26 21:49:58 +0800

    Added scrape environment with beautifulsoup4 installed. beautifulsoup_webScrape.py return href for page 2. OK

---
+++ beautifulsoup_webScrape.py
@@ -0,0 +1,12 @@
+#using www.crummy.com/software/BeautifulSoup/bs4/doc/
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+url = input('Enter - ').strip()
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
